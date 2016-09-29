4a4e85857f7b6b6c2bc8db5c479ed3fba2d8d4db
Durling <878877528@qq.com>
2016-09-29 13:44:29 +0800

    走通ajax post 七牛云上传图片 主要解决路径问题

--- public/js/basic/qiniu-upload.js
+++ public/js/basic/qiniu-upload.js
@@ -1,23 +1,35 @@
-
-
-
+var nowDateTimeStr = new Date().format("yyyyMMddhhmmss");
+var randomStr = Math.round(Math.random() * nowDateTimeStr);
+var randomOnce =  Math.random().toString(36).substr(20).toUpperCase();
+var uptimestr = nowDateTimeStr+'_'+randomOnce+'_'+randomStr;
+console.log(uptimestr);
 
 function sendForm(){
     var fd = new FormData();
-    fd.append("file",$('#file')[0].files[0]);
+    fd.append("upfiles",$('#file')[0].files[0]);
     fd.append("name",$('#name').val());
+    fd.append("nowDateTimeStr",nowDateTimeStr);
+    fd.append("randomOnce",randomOnce);
+
+    console.log(fd.get('upfiles'));
+    if (fd.get('upfiles')==undefined || fd.get('upfiles')=='undefined' || fd.get('upfiles')=='') {
+        return false;
+    }
+
     $.ajax({
         type:'post',
         dataType:'text',
+        data:fd,
+        async: false,
+        cache: false,
         processData: false,  // 告诉JSLite不要去处理发送的数据
         contentType: false,   // 告诉JSLite不要去设置Content-Type请求头
-        data:fd,
         url:'qiniu-upload/file-upload',
         success:function(data){
            console.log('success:',data)
         },
-        error:function(d){
-           console.log('error:',d)
+        error:function(err){
+           console.log('error:',err)
         }
     })
 }
--- routes/qiniu-upload.js
+++ routes/qiniu-upload.js
@@ -3,33 +3,24 @@
  */
 var express = require('express'),
 	router = express.Router(),
-	http = require('http'),
-	url = require('url'),
-	querystring = require('querystring');
-	request = require('request'),
 	mysql = require('mysql'),
-	path = require('path'),
 	config = require('../config.js'),
-	formidable = require('formidable'),
 	multiparty = require('multiparty'),
-
 	fs=require('fs'),
 	qiniu = require("qiniu");
 
 /**
  * 配日志
  */
-var logger = require('../log4js').logger;
-var logger_error = require('../log4js').logger_error;  
+// var logger = require('../log4js').logger;
+// var logger_error = require('../log4js').logger_error;  
 // logger.info("=== this is log from qiniu-upload.js ===");
 
 //需要填写你的 Access Key 和 Secret Key
 qiniu.conf.ACCESS_KEY = 'UtR9-061L8qFst2lvhiBR9Tc9E_u3sprXyOnTbSS';
 qiniu.conf.SECRET_KEY = 'huy_QYClFu6AEjVqc24cwX_98UjtWjKczjIAcjpF';
-
 //要上传的空间
 bucket = 'poemin';
-
 
 //构建上传策略函数
 function uptoken(bucket, key) {
@@ -37,103 +28,75 @@ function uptoken(bucket, key) {
   return putPolicy.token();
 }
 
+
 // 上传图片
 router.post('/file-upload', function (req,res) {
 	// console.log(req.url,req.body);
-	var n = req.body;
-	//上传到七牛后保存的文件名
-	key = n.fileName;
-	//生成上传 Token
-	token = uptoken(bucket, key);
-	console.log(token);
+	// var n = req.body;
 
-	// var insertSQL_params = [];	
-	var obj ={};
+	var fileNameList = [];
 	var form = new multiparty.Form();//实例一个multiparty
-	form.uploadDir = "./public/img/upload/";//设置文件储存路径
+	form.uploadDir = "public/img/qiniu-upload/";//设置文件储存路径
 	//开始解析前台传过来的文件
 	form.parse(req, function(err, fields, files) {
 		for (var item in fields){
-			// insertSQL_params.push(fields[item][0]);
 			console.log(fields[item][0]);
 		}
+		var nowDateTimeStr = fields.nowDateTimeStr;
+		var randomOnce = fields.randomOnce;
+		// console.log(nowDateTimeStr);
 		var filesTmp = JSON.stringify(files);
 		var pr = JSON.parse(filesTmp);
-		console.log(pr.upfiles.length);
+		// console.log(pr.upfiles.length);
 		if(err){
 			console.log('parse error: ' + err);
-		} else {
+		}else{
 			for (var i = 0 ; i < pr.upfiles.length ; i++) {
 				var inputFile = files.upfiles[i];//获取第一个文件
 				var finalname = inputFile.originalFilename;
-				// insertSQL_params.push(finalname);
-				var new_name = "./public/img/upload/"+finalname;//获取文件名
-				console.log(new_name);
+				var randomStr = Math.round(Math.random() * nowDateTimeStr);
+				finalname = nowDateTimeStr+'_'+randomOnce+'_'+randomStr;
+
+				//上传到七牛后保存的文件名
+				key = finalname;
+				// console.log(key);
+				//生成上传 Token
+				token = uptoken(bucket, key);
+				// console.log(token);
+
+				var new_name = form.uploadDir+finalname;//获取文件名
 				var old_name = inputFile.path;//获取文件路径
-				console.log(old_name);
-				fs.renameSync(old_name,new_name);	
-
-				//要上传文件的本地路径
-				filePath = String(new_name);
-
-				obj[filePath] = files[filePath];
-
+				// console.log(new_name,old_name);
+				fs.renameSync(old_name,new_name);
+				fileNameList.push(new_name);	
 			}
 		}
-		callback(err,obj);
+
+		// console.log(fileNameList);
+		for (var i = 0; i < fileNameList.length; i++) {
+			// console.log(fileNameList[i]);
+			filePath = fileNameList[i];
+			// 调用uploadFile上传
+			uploadFile(token, key, filePath);	
+		};
+
 	})
-
-
-// Parts are emitted when parsing the form 
-form.on('part', function(part) {
-  // You *must* act on the part by reading it 
-  // NOTE: if you want to ignore it, just call "part.resume()" 
-  if (!part.filename) {
-    // filename is not defined when this is a field and not a file 
-    console.log('got field named ' + part.name);
-    // ignore field's content 
-    part.resume();
-  }
-  if (part.filename) {
-    // filename is defined when this is a file 
-    count++;
-    console.log('got file named ' + part.name);
-    // ignore file's content here 
-    part.resume();
-  }
-  part.on('error', function(err) {
-    // decide what to do 
-  });
-});
-
-
-	form.on('close', function() {
-		console.log('Upload completed!');
-
-		// console.log(obj);
-		// filePath = obj.filePath;
-		// 调用uploadFile上传
-		// uploadFile(token, key, filePath);
-
-	});
-
-
-
-
 
 
 	//构造上传函数
 	function uploadFile(uptoken, key, localFile) {
-	  var extra = new qiniu.io.PutExtra();
+		// console.log(uptoken, key, localFile);
+	  	var extra = new qiniu.io.PutExtra();
 	    qiniu.io.putFile(uptoken, key, localFile, extra, function(err, ret) {
 	      if(!err) {
 	        // 上传成功， 处理返回值
-	        console.log(ret.hash, ret.key, ret.persistentId);   
-	        console.log(ret);
-			res.json(ret);    
+	        // console.log(ret);
+	        // console.log(ret.hash, ret.key, ret.persistentId);   
+			res.json(ret);
 	      } else {
 	        // 上传失败， 处理返回代码
-	        console.log(err);
+	        // console.log(err);
+			res.json(err);
 	      }
 	  });
 	}
@@ -141,21 +104,7 @@ form.on('part', function(part) {
 
 
 
-
-
-
-
-	
-
-
 });
-
-
-
-
-
-
-
 
 
 
